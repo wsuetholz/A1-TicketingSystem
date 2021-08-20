2f53288fbfe77b621cb713b42cc989ee0cc46320
William Suetholz <wj@suetholz.net>
2021-08-19 20:16:55 -0500

    Initial Console Application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A1_TicketingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
